089c809c82fc163577425ce474ca360765a11f4e
Emily <emiliabalazs.ki@gmail.com>
2024-11-19 13:15:34 +0100

    .

--- zadatak.py
+++ zadatak.py
@@ -1,28 +1,24 @@
 from flask import Flask, request, render_template_string
 import os
-import secrets
-from lxml import etree  # Korišćenje lxml biblioteke
+from lxml import etree
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # RCE ranjivost - unos komande
         command = request.form.get('command')
         if command:
             output = os.popen(command).read()
             return f'<pre>{output}</pre>'
 
-        # XXE ranjivost - upload XML fajla
         if 'file' in request.files:
             file = request.files['file']
             try:
-                # Parsiranje XML fajla sa lxml (ovde je ranjivo na XXE)
                 xml_content = file.read()
-                # Dodavanje zaštite protiv XXE
-                tree = etree.fromstring(xml_content, parser=etree.XMLParser(resolve_entities=False))
-                return 'XML procesiran bez XXE napada.'
+                # tu treba da je zastita od XXE
+                tree = etree.fromstring(xml_content, parser=etree.XMLParser(resolve_entities=True))
+                return 'XML procesiran (XXE omogućeno).'
             except etree.XMLSyntaxError as e:
                 return f'Greška u XML-u: {str(e)}', 400
 
